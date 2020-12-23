28186c8dc995adbe0d5cdb2dc29256a7d00db661
zhangyingjie1995 <372947506@qq.com>
2020-12-23 20:08:18 +0800

    12.23号

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,25 @@ import {
   TabbarItem,
   Icon,
   Field,
-  Button 
+  Button,
+  Tab, Tabs,
+  Image as VanImage,
+  Cell, CellGroup 
 } from 'vant';
 
 
 const app = createApp(App)
 app.use(Tabbar);
 app.use(TabbarItem);
+app.use(Tab);
+app.use(VanImage);
+app.use(Tabs)
 app.use(Field);
-app.use(router);
 app.use(Icon);
 app.use(Button);
+app.use(Cell);
+app.use(CellGroup);
+app.use(router);
 app.mount('#app')
 
 
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>这是我的</div>
+  <div class="mine">
+    <div class="title">
+      <van-image
+          round
+          width="59px"
+          height="59px"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+    </div>
+    <div>
+      <div class="wrop">
+        <div class="wrop-t">
+          <van-icon name="user-circle-o" />
+          <span class="txt">找设计师</span>
+        </div>
+         <div class="wrop-t">
+          <van-icon name="user-circle-o"/>
+          <span class="txt">提交需求</span>
+        </div>
+      </div>
+      <div class="main">
+        <div class="">
+          <van-cell title="单元格" is-link />
+          <van-cell title="单元格" is-link />
+        </div>
+       <div>
+          <van-cell title="单元格" is-link />
+          <van-cell title="单元格" is-link />
+       </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +39,43 @@ export default {
 }
 </script>
 
-<style>
 
+<style lang="scss" >
+ .mine{
+   background: #eee;
+   .title{
+     width: 100%;
+     height: 206px;
+     background: #F6483F;
+     position: relative;
+   }
+   .van-image--round{
+     position:absolute;
+     top:35px;
+     left:154.05px
+   } 
+  .wrop{
+    font-size: 12x;
+    display: flex;
+    justify-content: space-between;
+    text-align: center;
+    padding: 0 10px;
+     .wrop-t{
+       width: 175px;
+       height: 40px;
+       background:#fff ;
+       font-size: 16px;
+       color: #F6483F;
+       line-height: 40px;
+       display: flex;
+       text-align: center;
+       margin: 9px 0;
+       .van-icon{
+        font-size: 20px;
+        line-height: 40px;
+        margin:0 3px 0 35px;
+       }
+     }
+   }
+ }
 </style>
