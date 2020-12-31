badc70acd9cc3bd433e4f9c7390e0246c21683b6
zhangyingjie1995 <372947506@qq.com>
2020-12-31 09:11:35 +0800

    完整版

--- src/router/index.ts
+++ src/router/index.ts
@@ -86,7 +86,7 @@ const routes = [
 export const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
-  routes, // short for `routes: routes`
+  routes, // short for `routes: routes`1231231231
 })
 
 router.beforeEach((to, from, next) => {
